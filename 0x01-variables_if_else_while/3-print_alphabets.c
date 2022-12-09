32fc405dd96780bf947ea962b341fd444a72086d
Moses Ekwunife <mosesekwunife@yahoo.com>
2022-12-09 21:22:02 +0100

    var0x01-variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: Alway 0 (success)
+*/
+
+{
+	char lower_case; /*statement declaration*/
+	char upper_case;
+
+	/* Prints lower case alphabets*/
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	
+	/* Prints upper case alphabets*/
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
+		putchar(upper_case);
+	}
+
+	/* new line*/
+	putchar ('\n');
+
+	return (0)
+}
