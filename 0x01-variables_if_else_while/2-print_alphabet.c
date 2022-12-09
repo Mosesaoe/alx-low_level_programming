2f3eeb4a6645b6bac006dba758034fd9cc828213
Moses Ekwunife <mosesekwunife@yahoo.com>
2022-12-09 20:55:36 +0100

    var0x01-variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry 
+ * 
+ *  Return: Always 0 (success)
+ */ 
+ 
+ int main(void)
+ {
+ 	char letter; /*Declarling statement*/
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
