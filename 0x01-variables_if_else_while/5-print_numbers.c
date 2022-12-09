22887640eb1a547d172bf235cc705a85c3bcdac2
Moses Ekwunife <mosesekwunife@yahoo.com>
2022-12-09 21:54:30 +0100

    var0x01-variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(0)
+{
+	int i; /* Statement Declaaration*/
+	/*prints all single digit numbers of base 10 starting from 0*/
+	for (i =0 ; i < 10; i++)
+	{
+		printf("%i"); /*print i*/
+	}
+	
+	printf("\n"); /*new line*/
+
+	return (0);
+}
