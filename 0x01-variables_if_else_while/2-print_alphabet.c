e67b2ffa8ee27a326e58b873a0fdc8e91733d82c
Moses Ekwunife <mosesekwunife@yahoo.com>
2022-12-09 21:00:21 +0100

    var0x01-variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry 
+ * main - Entry
  * 
  *  Return: Always 0 (success)
- */ 
+ */
  
  int main(void)
- {
- 	char letter; /*Declarling statement*/
+{
+	char letter; /*Declarling statement*/
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
