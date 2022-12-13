cf0d72e35893522b0b2fc77418c745054fddf569
Moses Ekwunife <mosesekwunife@yahoo.com>
2022-12-13 19:53:20 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar"
+#include "main.h"
 
 /**
 * main - entry point
@@ -7,15 +7,16 @@
 */
 int main(void)
 {
-	char *sh = "_putchar"
-	
-	while (*sh)
-	{
-		_putchar (*sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	
+
 	return (0);
 }
 
